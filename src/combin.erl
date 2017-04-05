5c6f319bbc498334fcaced54b5a6f04b837bbb50
Jörgen Brandt <joergen.brandt@onlinehome.de>
2017-04-05 16:47:37 +0200

    Last minute changes.

--- src/combin.erl
+++ src/combin.erl
@@ -14,7 +14,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
-%% @author Jörgen Brandt <brandjoe@hu-berlin.de>
+%% @author Jörgen Brandt <joergen.brandt@onlinehome.de>
 
 -module( combin ).
 
