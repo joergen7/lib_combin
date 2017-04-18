50be5821d2f3f8e05c9171f14ab2dcbad532ff72
Jörgen Brandt <joergen.brandt@onlinehome.de>
2017-04-18 11:38:29 +0200

    Better error handling, full test coverage, factorial function added.

--- src/lib_combin.erl
+++ src/lib_combin.erl
@@ -22,7 +22,7 @@
 
 -module( lib_combin ).
 
--export( [cnr_all/1, cnr/2, pnr/1, permut_map/1, pick_from/1, vnr/2] ).
+-export( [cnr_all/1, cnr/2, pnr/1, permut_map/1, pick_from/1, vnr/2, fac/1] ).
 
 -ifdef( EUNIT ).
 -include_lib( "eunit/include/eunit.hrl" ).
@@ -53,6 +53,8 @@ cnr_all( SrcLst ) ->
 %% @doc Enumerates all combinations (order does not matter) of length `N'
 %%      without replacement by drawing elements from `SrcLst'.
 %%
+%%      Herein, `N` must be non-negative for the function clause to match.
+%%
 %%      Example:
 %%      ```
 %%      lib_combin:cnr( 2, [a,b,c] ).
@@ -60,7 +62,7 @@ cnr_all( SrcLst ) ->
 %%      '''
 -spec cnr( N::_, SrcLst::[_] ) -> [[_]].
 
-cnr( N, SrcLst ) when N >= 0 ->
+cnr( N, SrcLst ) when N >= 0, N =< length( SrcLst ) ->
 
   Cnr = fun
           Cnr( 0, _, Acc )     -> [Acc];
@@ -78,6 +80,8 @@ cnr( N, SrcLst ) when N >= 0 ->
 %% @doc Enumerates all variations (order matters) of length `N' without
 %%      replacement by drawing elements from `SrcLst'.
 %%
+%%      Herein, `N` must be non-negative for the function clause to match.
+%%
 %%      Example:
 %%      ```
 %%      lib_combin:vnr( 2, [a,b,c] ).
@@ -86,7 +90,7 @@ cnr( N, SrcLst ) when N >= 0 ->
 
 -spec vnr( N::_, SrcLst::[_] ) -> [[_]].
 
-vnr( N, SrcLst ) when N >= 0 ->
+vnr( N, SrcLst ) when N >= 0, N =< length( SrcLst ) ->
 
   Variat = fun
            Variat( 0, _, Acc ) ->
@@ -140,7 +144,7 @@ permut_map( SrcMap ) ->
 
   F = fun( _, VLst ) ->
         lists:usort( VLst )
-      end,
+  end,
 
   G = fun
         ( K, VLst, [] )  -> [#{ K => V } || V <- VLst];
@@ -161,10 +165,24 @@ permut_map( SrcMap ) ->
 -spec pick_from( [_] ) -> _.
 
 pick_from( SrcLst=[_|_] ) ->
-  SrcLst1 = lists:usort( SrcLst ),
-  N = rand:uniform( length( SrcLst1 ) ),
-  lists:nth( N, SrcLst1 ).
+  N = rand:uniform( length( SrcLst ) ),
+  lists:nth( N, SrcLst ).
 
+%% @doc The factorial function.
+%%
+%% Example:
+%% ```
+%% factorial( 4 ).
+%% 24
+%% '''
+
+-spec fac( non_neg_integer() ) -> pos_integer().
+
+fac( 0 ) ->
+  1;
+
+fac( N ) when N > 0 ->
+  N*fac( N-1 ).
 
 %%====================================================================
 %% Internal functions
@@ -184,13 +202,101 @@ cnr_n_returns_one_elements_test() ->
   SrcLst = [a, b, c, d, e, f],
   ?assertEqual( 1, length( cnr( 6, SrcLst ) ) ).
 
+cnr_zero_is_degenerate_but_valid_test() ->
+  SrcLst = [a, b, c, d, e, f],
+  ?assertEqual( [[]], cnr( 0, SrcLst ) ).
+
 cnr_is_robust_wrt_duplicates_test() ->
   SrcLst = [a, b, c, d, e, f, f, e, b],
   ?assertEqual( 1, length( cnr( 6, SrcLst ) ) ).
 
+cnr_neg_throws_error_test() ->
+  SrcLst = [a, b, c, d, e, f],
+  ?assertError( function_clause, cnr( -1, SrcLst ) ).
+
+cnr_too_large_throws_error_test() ->
+  SrcLst = [a, b, c, d, e, f],
+  ?assertError( function_clause, cnr( 7, SrcLst ) ).
+
 cnr_all_test() ->
   SrcLst = [a,b,c],
   ?assertEqual( 1+3+3+1, length( cnr_all( SrcLst ) ) ).
+
+vnr_one_returns_n_elements_test() ->
+  SrcLst = [a, b, c, d, e, f],
+  ?assertEqual( 6, length( vnr( 1, SrcLst ) ) ).
+
+vnr_n_returns_one_elements_test() ->
+  SrcLst = [a, b, c, d, e, f],
+  ?assertEqual( fac( 6 ), length( vnr( 6, SrcLst ) ) ).
+
+vnr_zero_is_degenerate_but_valid_test() ->
+  SrcLst = [a, b, c, d, e, f],
+  ?assertEqual( [[]], vnr( 0, SrcLst ) ).
+
+vnr_is_robust_wrt_duplicates_test() ->
+  SrcLst = [a, b, c, d, e, f, f, e, b],
+  ?assertEqual( fac( 6 ), length( vnr( 6, SrcLst ) ) ).
+
+vnr_neg_throws_error_test() ->
+  SrcLst = [a, b, c, d, e, f],
+  ?assertError( function_clause, vnr( -1, SrcLst ) ).
+
+vnr_too_large_throws_error_test() ->
+  SrcLst = [a, b, c, d, e, f],
+  ?assertError( function_clause, vnr( 7, SrcLst ) ).
+
+pnr_is_robust_wrt_duplicates_test() ->
+  SrcLst = [a, b, c, d, e, f, f, e, b],
+  ?assertEqual( fac( 6 ), length( pnr( SrcLst ) ) ).
+
+burger_restaurant_example_test() ->
+
+  IngredientMap = #{ sauce => [ketchup, mayo],
+                     bread => [sesame, plain],
+                     meat => [beef, chicken, mutton] },
+
+  ExpectedLst = [#{bread => plain,meat => beef,sauce => ketchup},
+                 #{bread => sesame,meat => beef,sauce => ketchup},
+                 #{bread => plain,meat => chicken,sauce => ketchup},
+                 #{bread => sesame,meat => chicken,sauce => ketchup},
+                 #{bread => plain,meat => mutton,sauce => ketchup},
+                 #{bread => sesame,meat => mutton,sauce => ketchup},
+                 #{bread => plain,meat => beef,sauce => mayo},
+                 #{bread => sesame,meat => beef,sauce => mayo},
+                 #{bread => plain,meat => chicken,sauce => mayo},
+                 #{bread => sesame,meat => chicken,sauce => mayo},
+                 #{bread => plain,meat => mutton,sauce => mayo},
+                 #{bread => sesame,meat => mutton,sauce => mayo}],
+
+  Result =  lib_combin:permut_map( IngredientMap ),
+
+  ?assertEqual( ExpectedLst, Result ).
+
+pick_from_singleton_list_returns_only_element_test() ->
+  ?assertEqual( a, pick_from( [a] ) ).
+
+pick_from_empty_throws_error_test() ->
+  ?assertError( function_clause, pick_from( [] ) ).
+
+fac_zero_is_one_test() ->
+  ?assertEqual( 1, fac( 0 ) ).
+
+fac_one_is_one_test() ->
+  ?assertEqual( 1, fac( 1 ) ).
+
+fac_two_is_two_test() ->
+  ?assertEqual( 2, fac( 2 ) ).
+
+fac_three_is_six_test() ->
+  ?assertEqual( 6, fac( 3 ) ).
+
+fac_four_is_24_test() ->
+  ?assertEqual( 24, fac( 4 ) ).
+
+fac_neg_throws_error_test() ->
+  ?assertError( function_clause, fac( -1 ) ).
+
 
 -endif.
 
